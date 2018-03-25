df3b56c8f2a5a82356fd02cdc178f113181f2162
seanDoJo <sedo8743@colorado.edu>
2018-03-25 11:42:38 -0600

    trying test

--- e2e/tests.py
+++ e2e/tests.py
@@ -1,8 +1,12 @@
 import requests
 
-r = requests.post("http://ec2-18-188-95-46.us-east-2.compute.amazonaws.com:8080/create",json={"host":"The Walrus"})
+d = "EAADPy1iOzAQBAPMKrmWG6GAqO2PYpRxZAsBfVdpWHxckPUNT64XEtcrrMQ0oJRatXZAZBUA0lKJNkgtUmLD80A1yEtQyxtjtIsZAprawshiajAl21lEH0pcju4kZBQaHXKWTtTqAFLecwvnAitZAe1fpclopcWrQVSpu9IV2w0Bj6R9e6XEpi4vLjOK7riNpcZD"
+
+r = requests.post("http://ec2-18-188-95-46.us-east-2.compute.amazonaws.com:8080/create/{}".format(d),json={"host":"The Walrus"})
 
 print(r.json())
+
+exit(0)
 
 r = requests.post("http://ec2-18-188-95-46.us-east-2.compute.amazonaws.com:8080/get",json={"host":"The Walrus"})
 
