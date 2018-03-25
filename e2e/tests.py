455b7e70c2cd7406331f9f12db0ef3568a21aca7
seanDoJo <sedo8743@colorado.edu>
2018-03-25 11:44:39 -0600

    better access token

--- e2e/tests.py
+++ e2e/tests.py
@@ -1,6 +1,7 @@
 import requests
+import os
 
-d = "EAADPy1iOzAQBAPMKrmWG6GAqO2PYpRxZAsBfVdpWHxckPUNT64XEtcrrMQ0oJRatXZAZBUA0lKJNkgtUmLD80A1yEtQyxtjtIsZAprawshiajAl21lEH0pcju4kZBQaHXKWTtTqAFLecwvnAitZAe1fpclopcWrQVSpu9IV2w0Bj6R9e6XEpi4vLjOK7riNpcZD"
+d = os.getenv("ACCESS_TOKEN", "none")
 
 r = requests.post("http://ec2-18-188-95-46.us-east-2.compute.amazonaws.com:8080/create/{}".format(d),json={"host":"The Walrus"})
 
