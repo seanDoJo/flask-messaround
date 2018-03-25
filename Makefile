ac3d04974be2ee9e759bb4c72fdb6af9b5fe32ab
EC2 Default User <ec2-user@ip-172-31-37-250.us-east-2.compute.internal>
2018-03-25 21:04:42 +0000

    updating architecture

--- Makefile
+++ Makefile
@@ -1,59 +1,9 @@
-.PHONY: nginx, e2e
-
-status_redis :
-	cat /var/log/redis_6379.log
-
-stop_redis :
-	/etc/init.d/redis_6379 stop
-
-start_redis :
-	/etc/init.d/redis_6379 start
-
-e2e : 
-	export FLASK_APP=utils/queue.py
-	flask run &
-	uwsgi --ini test.ini
-
-run :
-	export FLASK_APP=utils/queue.py
-	flask run &
-	uwsgi --ini config.ini
-
-go : nginx py_deps conf_redis
-	export FLASK_APP=utils/queue.py
-	flask run &
-	uwsgi --ini config.ini
-
+.PHONY: nginx
 nginx :
 	yum install nginx
 	service nginx stop
 	cp nginx/nginx.conf.default /etc/nginx/nginx.conf
 	service nginx start
-
-py_deps :
-	yum install gcc
-	yum install python35
-	yum install python35-pip
-	yum install python35-devel
-	pip-3.5 install redis
-	pip-3.5 install flask
-	pip-3.5 install uwsgi 
-
-conf_redis : build_redis
-	mkdir /etc/redis
-	mkdir /var/redis
-	cp redis/redis_init_script /etc/init.d/redis_6379
-	cp redis/redis.conf /etc/redis/6379.conf
-	mkdir /var/reds/6379
-	/etc/init.d/redis_6379 start
-	rm -rf redis-stable
-
-build_redis : 
-	wget http://download.redis.io/redis-stable.tar.gz
-	tar xzvf redis-stable.tar.gz
-	rm redis-stable.tar.gz
-	cd redis-stable/deps && make hiredis lua jemalloc linenoise
-	cd redis-stable && make && make install
 
 clean :
 	rm *.gz
--- e2e/tests.py
+++ e2e/tests.py
@@ -2,21 +2,21 @@ import requests
 import os
 
 d = os.getenv("ACCESS_TOKEN", "none")
-
+"""
 r = requests.post("http://ec2-18-188-95-46.us-east-2.compute.amazonaws.com:8080/create/{}".format(d),json={"host":"The Walrus"})
-
 print(r.json())
 
 r = requests.post("http://ec2-18-188-95-46.us-east-2.compute.amazonaws.com:8080/get/{}".format(d),json={"host":"The Walrus"})
 
 print(r.json())
 
-r = requests.get("http://ec2-18-188-95-46.us-east-2.compute.amazonaws.com:8080/lookup/{}/walr".format(d))
-print(r.json())
+r = requests.post("http://ec2-18-188-95-46.us-east-2.compute.amazonaws.com:8080/listorders/{}".format(d), json={'host':'The Walrus'})
 
-exit(0)
+print(r.json())
+"""
+
 r = requests.post(
-        "http://ec2-18-188-95-46.us-east-2.compute.amazonaws.com:8080/put",
+        "http://ec2-18-188-95-46.us-east-2.compute.amazonaws.com:8080/put/{}".format(d),
         json={
             "host":"The Walrus",
             "categories" : {
@@ -29,8 +29,29 @@ r = requests.post(
             }
         }
 )
+print(r)
+print(r.json())
+
+r = requests.post(
+        "http://ec2-18-188-95-46.us-east-2.compute.amazonaws.com:8080/place/{}".format(d),
+        json={
+            "host" : "The Walrus",
+            "order" : {
+                "Sean Donohoe" : {
+                    "Left Hand Milk Stout" : {
+                        "quantity" : 2
+                    }
+                }
+            }
+        }
+)
+print(r.json())
+
+r = requests.post("http://ec2-18-188-95-46.us-east-2.compute.amazonaws.com:8080/listorders/{}".format(d), json={'host':'The Walrus'})
 
 print(r.json())
+
+exit(0)
 
 r = requests.post(
         "http://ec2-18-188-95-46.us-east-2.compute.amazonaws.com:8080/get",
