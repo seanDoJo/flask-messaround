4178c0a39ad8af9ff6a812b07c3be8f17cd4915b
seanDoJo <sddonohoe@msn.com>
2018-03-28 16:58:45 +0000

    working firewall rules

--- uwsgi/rules.sh
+++ uwsgi/rules.sh
@@ -8,6 +8,7 @@ iptables -A INPUT -i eth0 -p tcp -s 172.31.37.250 --dport 8000 -j ACCEPT
 iptables -A INPUT -i eth0 -p tcp --sport 443 -m state --state ESTABLISHED -j ACCEPT
 iptables -A INPUT -i eth0 -p tcp --sport 80 -m state --state ESTABLISHED -j ACCEPT
 iptables -A INPUT -i eth0 -p udp --sport 53 -j ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
 iptables -P INPUT DROP
 
 iptables -A OUTPUT -o eth0 -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
@@ -16,4 +17,5 @@ iptables -A OUTPUT -o eth0 -p tcp -d 172.31.37.250 --sport 8000 -j ACCEPT
 iptables -A OUTPUT -o eth0 -p tcp --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -o eth0 -p tcp --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -o eth0 -p udp --dport 53 -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
 iptables -P OUTPUT DROP
