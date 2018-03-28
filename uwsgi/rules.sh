4ded33c82f199847a83170a92ce1c635f9b0f8cd
seanDoJo <sddonohoe@msn.com>
2018-03-28 16:41:49 +0000

    better firewall rules

--- uwsgi/rules.sh
+++ uwsgi/rules.sh
@@ -3,16 +3,16 @@ iptables -P OUTPUT ACCEPT
 iptables -F
 
 iptables -A INPUT -i eth0 -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A INPUT -i eth0 -p tcp -s 172.31.37.250 --dport 8080 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A INPUT -i eth0 -p tcp -s 172.31.37.250 --dport 8000 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -i eth0 -p tcp -s 172.31.37.250 --dport 8080 -j ACCEPT
+iptables -A INPUT -i eth0 -p tcp -s 172.31.37.250 --dport 8000 -j ACCEPT
 iptables -A INPUT -i eth0 -p tcp --sport 443 -m state --state ESTABLISHED -j ACCEPT
 iptables -A INPUT -i eth0 -p tcp --sport 80 -m state --state ESTABLISHED -j ACCEPT
 iptables -A INPUT -i eth0 -p udp --sport 53 -j ACCEPT
 iptables -P INPUT DROP
 
 iptables -A OUTPUT -o eth0 -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -o eth0 -p tcp -d 172.31.37.250 --sport 8080 -m state --state ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -o eth0 -p tcp -d 172.31.37.250 --sport 8000 -m state --state ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o eth0 -p tcp -d 172.31.37.250 --sport 8080 -j ACCEPT
+iptables -A OUTPUT -o eth0 -p tcp -d 172.31.37.250 --sport 8000 -j ACCEPT
 iptables -A OUTPUT -o eth0 -p tcp --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -o eth0 -p tcp --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -o eth0 -p udp --dport 53 -j ACCEPT
