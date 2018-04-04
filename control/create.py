28abec3ab279c808556cc457fa31e443200d2e4b
seanDoJo <sedo8743@colorado.edu>
2018-04-04 13:40:58 -0600

    rearchitecting

--- control/create.py
+++ control/create.py
@@ -15,7 +15,6 @@ def hostify(h):
     return re.sub(r"\s+", "-", s)
 
 def spawn(securityGroupID, subnetID, userData):
-    print(userData)
     front = Popen(
         [
             'aws',
@@ -94,30 +93,16 @@ export HOST="{}"
 cd /home/ec2-user/flask-messaround/uwsgi && make go
 """.format(os.environ['APP_ID'], os.environ['APP_SECRET'], host+str(number))
 
-front_ip, front_id = tempAndSpawn(frontData, 'sg-0cae6e997c3bbcb91', 'subnet-3ee3df73')
+#front_ip, front_id = tempAndSpawn(frontData, 'sg-0cae6e997c3bbcb91', 'subnet-3ee3df73')
 queue_ip, queue_id = tempAndSpawn(queueData, 'sg-0b4efff588cd6b30e', 'subnet-3ee3df73')
 
-nginxData = """
-#!/bin/bash
-
-yum update -y
-yum install -y git python35
-cd /home/ec2-user && git clone https://github.com/seanDoJo/flask-messaround.git
-mkdir /home/ec2-user/flask-messaround/nginx/conf
-chown -R ec2-user /home/ec2-user/flask-messaround
-cd /home/ec2-user/flask-messaround/nginx && /usr/bin/python35 gen_conf.py {} {}
-chown -R ec2-user /home/ec2-user/flask-messaround
-cd /home/ec2-user/flask-messaround && make nginx
-""".format(front_ip, queue_ip)
-
-nginx_ip, nginx_id = tempAndSpawn(nginxData, 'sg-0a0a33d5d5babbeab', 'subnet-3ee3df73')
-
 r = requests.post(
-    "http://ec2-52-14-58-91.us-east-2.compute.amazonaws.com:8000/addconf",
+    "http://ec2-52-14-58-91.us-east-2.compute.amazonaws.com:8000/addqueue",
     json={
         'url': "{}{}".format(hostify(host), number),
-        'ip_addr': nginx_ip,
-        'port': 8080
+        'ip_addr': queue_ip,
+        'read_port': 8000,
+        'write_port': 8080
     }
 )
 print(r)
--- proxy/server.py
+++ proxy/server.py
@@ -3,23 +3,42 @@ from subprocess import Popen, PIPE
 
 app = Flask(__name__)
 
-@app.route('/addconf', methods=['POST'])
+@app.route('/addqueue', methods=['POST'])
 def addconf():
     path = request.json['url']
     ip = request.json['ip_addr']
-    port = request.json['port']
+    read_port = request.json['read_port']
+    write_port = request.json['write_port']
 
     with open("conf/{}.conf".format(path), 'w+') as f:
         f.write(
 """
-location /{} {{
-    proxy_pass http://{}:{}/;
+location /{}/update {{
+    include uwsgi_params;
+    uwsgi_pass {}:{}/update;
+
+    proxy_redirect_off;
     proxy_set_header Host $host;
     proxy_set_header X-Real-IP $remote_addr;
     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
     proxy_set_header X-Forwarded-Host $server_name;
 }}
-""".format(path, ip, port)
+""".format(path, ip, write_port)
+        )
+
+    f.write(
+"""
+location /{}/orders {{
+    include uwsgi_params;
+    uwsgi_pass {}:{}/orders;
+
+    proxy_redirect_off;
+    proxy_set_header Host $host;
+    proxy_set_header X-Real-IP $remote_addr;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Host $server_name;
+}}
+""".format(path, ip, read_port)
         )
 
     with open('lastupdate.log', 'w+') as log:
